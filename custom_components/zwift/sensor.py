41a95047cf78ab71217b48819f014745511a6e1a
Greg Sarjeant <1686767+gsarjeant@users.noreply.github.com>
2025-01-25 20:41:31 -0500

    Add .gitignore. Add imperial measurements and diagnostic logging.

--- custom_components/zwift/sensor.py
+++ custom_components/zwift/sensor.py
@@ -401,6 +401,29 @@ class ZwiftData:
                         # [TODO] is this correct regardless of metric/imperial? Correct regardless of world?
                         altitude = (float(player_state.altitude) - 9000) / 2
                         distance = float(player_state.distance)
+                        speed = player_state.speed / 1000000.0
+                        _LOGGER.debug("Zwift altitude: {}".format(
+                            altitude))
+                        _LOGGER.debug("Zwift diistance: {}".format(
+                            distance))
+                        _LOGGER.debug("Zwift speed: {}".format(
+                            speed))
+
+                        # Conversions from metric to imperial units
+                        METERS_TO_MILES = 0.000621371
+                        METERS_TO_FEET = 3.28084
+
+                        altitude_imperial = float(altitude * METERS_TO_FEET)
+                        distance_imperial = float(distance * METERS_TO_MILES)
+                        speed_imperial = float(speed * METERS_TO_MILES * 1000)
+                        useMetric = player_profile.get("useMetric", False)
+                        _LOGGER.debug("Zwift altitude (imperial): {}".format(
+                            altitude_imperial))
+                        _LOGGER.debug("Zwift distance (imperial): {}".format(
+                            distance_imperial))
+                        _LOGGER.debug("Zwift speed (imperial): {}".format(
+                            speed_imperial))
+
                         gradient = self.players[player_id].data.get(
                             'gradient', 0)
                         rideons = latest_activity.get('activityRideOnCount', 0)
@@ -410,10 +433,20 @@ class ZwiftData:
                                 'rideons': rideons
                             })
                         if self.players[player_id].data.get('distance', 0) > 0:
-                            delta_distance = distance - \
-                                self.players[player_id].data.get('distance', 0)
-                            delta_altitude = altitude - \
-                                self.players[player_id].data.get('altitude', 0)
+                            previous_distance = self.players[player_id].data.get('distance', 0)
+                            previous_altitude = self.players[player_id].data.get('altitude', 0)
+                            _LOGGER.debug("Zwift previous altitude: {}".format(
+                                previous_altitude))
+                            _LOGGER.debug("Zwift previous distance: {}".format(
+                                previous_distance))
+
+                            if useMetric:
+                                delta_distance = distance - previous_distance
+                                delta_altitude = altitude - previous_altitude
+                            else:
+                                delta_distance = distance_imperial - previous_distance
+                                delta_altitude = altitude_imperial - previous_altitude
+
                             if delta_distance > 0:
                                 gradient = delta_altitude / delta_distance
                         data.update({
@@ -421,9 +454,9 @@ class ZwiftData:
                             'heartrate': int(float(player_state.heartrate)),
                             'cadence': int(float(player_state.cadence)),
                             'power': int(float(player_state.power)),
-                            'speed': player_state.speed / 1000000.0,
-                            'altitude': altitude,
-                            'distance': distance,
+                            'speed': speed if useMetric else speed_imperial,
+                            'altitude': altitude if useMetric else altitude_imperial,
+                            'distance': distance if useMetric else distance_imperial,
                             'gradient': gradient,
                             'rideons': rideons
                         })
